580caa836e38d4555ee118ee73f5b3d78bf917f2
Jose <86626843+JOSEDOWSK1@users.noreply.github.com>
2021-10-03 16:53:48 -0500

    Add files via upload

---
+++ aplicacion.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request # Import the class `Flask` from the `flask` module, written by someone else.
+
+app = Flask(__name__) # Instantiate a new web application called `app`, with `__name__` representing the current file
+
+
+@app.route("/") 
+def index():
+	return render_template(index.html)
+@app.route("/hola", methonds=["POST")
+def hola():
+	name = request.form.get("name")
+	return render_template("hola.html",name=name)
+
